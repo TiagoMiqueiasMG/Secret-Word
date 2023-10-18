64aaf3511431658168bbb9f101b3e8c78832b079
TiagoMiqueiasDesenvolvedor <tiago.rodrigues.1338413@sga.pucminas.br>
2023-10-18 17:44:56 -0300

    creating list for the game

---
+++ secretword/src/data/words.js
@@ -0,0 +1,12 @@
+const wordList = {
+  carro: ["Uno", "Gol", "Ferrari", "Pneu", "Capô"],
+  filmes: ["Matrix", "Pulp Fiction", "Cidadão Kane", "Avatar", "E.T."],
+  cores: ["Azul", "Vermelho", "Verde", "Amarelo", "Roxo"],
+  animais: ["Elefante", "Pinguim", "Girafa", "Tigre", "Borboleta"],
+  comida: ["Pizza", "Sushi", "Chocolate", "Salada", "Hamburguer"],
+  cidades: ["Nova York", "Paris", "Tóquio", "Roma", "Sydney"],
+  programacao: ["JavaScript", "Python", "Java", "C", "HTML"],
+  esportes: ["Futebol", "Basquete", "Tênis", "Surfe", "Esqui"],
+  instrumentos: ["Guitarra", "Piano", "Violino", "Bateria", "Saxofone"],
+  profissoes: ["Médico", "Engenheiro", "Artista", "Advogado", "Professor"],
+};
