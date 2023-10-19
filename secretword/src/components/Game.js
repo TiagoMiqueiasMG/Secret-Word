f9309465184a343a909dec6a38e26a9ece2d73f1
TiagoMiqueiasDesenvolvedor <tiago.rodrigues.1338413@sga.pucminas.br>
2023-10-19 08:38:28 -0300

    Creacte Game and GameOver and setting up the call

---
+++ secretword/src/components/Game.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Game.js'
+
+const Game = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Game
---
+++ secretword/src/components/GameOver.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './GameOver.css'
+
+const GameOver = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
+export default GameOver
