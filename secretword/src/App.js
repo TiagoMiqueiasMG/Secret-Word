88ca06ad8464a1bfc16a197a5f50fd6ef3a3b055
TiagoMiqueiasDesenvolvedor <tiago.rodrigues.1338413@sga.pucminas.br>
2023-10-19 08:18:11 -0300

    configuring progression stages

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -10,6 +10,12 @@ import {wordsList} from "./data/words"
 // Components
 import StartScreen from './components/StartScreen';
 
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id:3, name: "end"}
+]
+
 function App() {
   return (
     <div className="App">
