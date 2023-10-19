87984d708e5a37069270e77d6d9d68bd751d5e77
TiagoMiqueiasDesenvolvedor <tiago.rodrigues.1338413@sga.pucminas.br>
2023-10-19 12:47:02 -0300

    sett and verify letter

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -71,8 +71,8 @@ function App() {
   };
 
   // Process the letter input
-  const verifyLetter = () =>{
-    setGameStage(stages[2].name)
+  const verifyLetter = (letter) =>{
+    console.log(letter);
   } 
 
   // Reestart the game
--- secretword/src/components/Game.js
+++ secretword/src/components/Game.js
@@ -19,6 +19,8 @@ const Game = ({
     e.preventDefault();
 
     verifyLetter(letter);
+
+    setLetter("");
   }
 
   return (
