bce31124501227a9937326f78f733b4837aadf57
TiagoMiqueiasDesenvolvedor <tiago.rodrigues.1338413@sga.pucminas.br>
2023-10-20 15:39:44 -0300

    data

--- secretword/src/data/words.js
+++ secretword/src/data/words.js
@@ -1,9 +1,9 @@
 export const wordsList = {
   Carro: ["Uno", "Gol", "Ferrari", "Pulse", "Mobi", "Jeep"],
-  filmes: ["Matrix", "Pulp Fiction", "Lagoa azul", "Avatar", "Moana"],
+  filmes: ["Matrix", "Evolucao", "Lagoa azul", "Avatar", "Moana"],
   cores: ["Azul", "Vermelho", "Verde", "Amarelo", "Roxo"],
   animais: ["Elefante", "Pinguim", "Girafa", "Tigre", "Borboleta"],
-  comida: ["Pizza", "Sushi", "Chocolate", "Salada", "Pastel"],
+  comida: ["Pizza", "Sushi", "Salada", "Pastel"],
   cidades: ["Nova York", "Paris", "Belo Horizonte", "Roma", "Sydney", "Rio acima"],  
   esportes: ["Futebol", "Basquete", "Corrida", "Surfe", "Esqui"],
   instrumentos: ["Guitarra", "Piano", "Violino", "Bateria", "Saxofone", "Piano"],
