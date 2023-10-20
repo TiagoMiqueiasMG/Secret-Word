f110a7d48eeca414d1ed7204cbd60af9cf36ba44
TiagoMiqueiasDesenvolvedor <tiago.rodrigues.1338413@sga.pucminas.br>
2023-10-20 11:17:37 -0300

    update data

--- secretword/src/data/words.js
+++ secretword/src/data/words.js
@@ -1,12 +1,12 @@
 export const wordsList = {
-  carro: ["Uno", "Gol", "Ferrari", "Pneu", "Capô"],
-  filmes: ["Matrix", "Pulp Fiction", "Cidadão Kane", "Avatar", "E.T."],
+  carro: ["Uno", "Gol", "Ferrari", "Pneu", "Porta", "Motor"],
+  filmes: ["Matrix", "Pulp Fiction", "Lagoa azul", "Avatar", "De volta para o futuro"],
   cores: ["Azul", "Vermelho", "Verde", "Amarelo", "Roxo"],
   animais: ["Elefante", "Pinguim", "Girafa", "Tigre", "Borboleta"],
   comida: ["Pizza", "Sushi", "Chocolate", "Salada", "Hamburguer"],
-  cidades: ["Nova York", "Paris", "Tóquio", "Roma", "Sydney"],
+  cidades: ["Nova York", "Paris", "Belo Horizonte", "Roma", "Sydney", "Rio acima"],
   programacao: ["JavaScript", "Python", "Java", "C", "HTML"],
   esportes: ["Futebol", "Basquete", "Tênis", "Surfe", "Esqui"],
   instrumentos: ["Guitarra", "Piano", "Violino", "Bateria", "Saxofone"],
-  profissoes: ["Médico", "Engenheiro", "Artista", "Advogado", "Professor"],
+  profissoes: ["Programador", "Engenheiro", "Artista", "Advogado", "Professor"],
 };
